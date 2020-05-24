312ba11e83a43fd310d6dabc2d156fd42421e0e5
Jing Li <jing_li1998@hotmail.com>
2020-05-24 14:51:33 +1000

    move tailwind config to gridsome config

--- gridsome.config.js
+++ gridsome.config.js
@@ -3,22 +3,31 @@
 
 // Changes here require a server restart.
 // To restart press CTRL + C in terminal and run `gridsome develop`
-const BundleAnalyzerPlugin = require("webpack-bundle-analyzer")
-  .BundleAnalyzerPlugin;
+// const BundleAnalyzerPlugin = require("webpack-bundle-analyzer")
+//   .BundleAnalyzerPlugin;
 
 module.exports = {
   siteName: "Gridsome",
   plugins: [
     {
       use: "gridsome-plugin-tailwindcss",
+      options: {
+        purgeConfig: {
+          content: ["src/assets/**/*.css", "src/**/*.vue", "src/**/*.js"],
+          options: {
+            whitelist: ["svg-inline--fa"],
+            whitelistPatterns: [/svg.*/, /fa.*/, /shiki/, /fa-$/],
+          },
+        },
+      },
     },
   ],
 
-  chainWebpack: (config) => {
-    if (process.env.NODE_ENV === "production") {
-      config
-        .plugin("BundleAnalyzerPlugin")
-        .use(BundleAnalyzerPlugin, [{ analyzerMode: "static" }]);
-    }
-  },
+  // chainWebpack: (config) => {
+  //   if (process.env.NODE_ENV === "production") {
+  //     config
+  //       .plugin("BundleAnalyzerPlugin")
+  //       .use(BundleAnalyzerPlugin, [{ analyzerMode: "static" }]);
+  //   }
+  // },
 };
