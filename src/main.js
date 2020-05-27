3df40a89c249eacce1c0b7d1d4c32d469ce6a696
Jing Li <jing_li1998@hotmail.com>
2020-05-27 16:13:04 +1000

    remove files

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 // This is the main.js file. Import global CSS and scripts here.
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 
-import DefaultLayout from "~/layouts/Default.vue";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { config, library } from "@fortawesome/fontawesome-svg-core";
 import {
@@ -27,5 +26,4 @@ export default function (Vue, { head }) {
   );
   // Set default layout as a global component
   Vue.component("font-awesome", FontAwesomeIcon);
-  Vue.component("Layout", DefaultLayout);
 }
