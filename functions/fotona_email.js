778be09d24c73525e1c46ecdbff091f0173131b3
Jing Li <z5112692@ad.unsw.edu.au>
2021-03-20 22:28:40 +1100

    Fix function

--- functions/fotona_email.js
+++ functions/fotona_email.js
@@ -1,7 +1,7 @@
-exports.handler = async(event, context) {
+exports.handler = async function (event, context) {
   return {
     statusCode: 200,
-    body: JSON.stringify({message: "Function invoked"})
-    received: JSON.stringify(event.body)
-  }
-}
+    body: JSON.stringify({ message: "Function invoked" }),
+    received: JSON.stringify(event.body),
+  };
+};
